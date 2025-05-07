ceccc598735b024cb54e85eb797a09fe43740aa3
Flavio Del Mestre <29498370+fdmgit@users.noreply.github.com>
2025-05-07 18:18:32 +0200

    Update end_inst.sh

--- end_inst.sh
+++ end_inst.sh
@@ -1,11 +1,11 @@
 # this is a part of the installation script
 # will be automatically include in the overall installation script
 
-#inst_php82
-#inst_webmin
-#inst_certbot
-#inst_f2b_jails
-#inst_locate
+inst_php82
+inst_webmin
+inst_certbot
+inst_f2b_jails
+inst_locate
 cd /root
 #wget https://raw.githubusercontent.com/fdmgit/asterisk/main/inst_logo
 closing_msg
