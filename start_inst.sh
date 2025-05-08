4bc3e5ec8add33ea43020e0876dd85d23c90a125
Flavio Del Mestre <29498370+fdmgit@users.noreply.github.com>
2025-05-08 12:04:19 +0200

    Update start_inst.sh

--- start_inst.sh
+++ start_inst.sh
@@ -140,7 +140,8 @@ server_env () {
     cp bashrc.ini /root/.bashrc
     cp bashrc.ini /etc/skel/.bashrc
     rm /root/bashrc.ini
-    . .bashrc
+    echo "export PATH="$PATH:/root/.local/bin:/snap/bin"  >> .bashrc
+    Source ./.bashrc
     apt install curl sudo -y
 
     ###################################
